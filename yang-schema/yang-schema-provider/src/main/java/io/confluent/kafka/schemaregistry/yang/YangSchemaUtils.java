6557ec4a7ee364f9d790b6d9af837e2871584c28
yangpei1214 <yangpei1214@gmail.com>
2024-02-05 09:12:11 -0800

    .

--- yang-schema/yang-schema-provider/src/main/java/io/confluent/kafka/schemaregistry/yang/YangSchemaUtils.java
+++ yang-schema/yang-schema-provider/src/main/java/io/confluent/kafka/schemaregistry/yang/YangSchemaUtils.java
@@ -1,5 +1,6 @@
 /*
  * Copyright 2023 Swisscom (Schweiz) AG.
+ * Copyright 2023 Confluent Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
